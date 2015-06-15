3140cb396b8be97623346a71b79d81746fb6d198
strengthening <ducg@foxmail.com>
2015-06-15 20:57:01 +0800

    change settings.py

--- ss_panel_django/settings.py
+++ ss_panel_django/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminmgr',
+    'users',
+    'nodes'
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,13 +78,22 @@ WSGI_APPLICATION = 'ss_panel_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ss_panel_django',
+        'USER': 'root',
+        'PASSWORD': 'dcg8280828',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -100,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 
 
