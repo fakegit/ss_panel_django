b62750b1314fb2ceb9f85e297111ec19de9ab86c
strengthening <ducg@foxmail.com>
2015-06-16 20:29:52 +0800

    add utls

--- utils/strutils.py
+++ utils/strutils.py
@@ -14,8 +14,14 @@ class Strutils(object):
     @staticmethod
     def isEmail(ustr):
         flag = False
-        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", ustr) != None:
             flag = True
         return flag
+
+
+
+if __name__== '__main__':
+    print Strutils.isEmail("ducg@foxmail.com")
+    #print 'fdsfsd'
 
 
