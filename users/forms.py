de384bf17b917cacd4056a0694bbb0c59d1e88af
strengthening <ducg@foxmail.com>
2015-07-12 01:06:31 +0800

    a

--- users/forms.py
+++ users/forms.py
@@ -8,15 +8,27 @@ import json
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(label='用户名',max_length=30,help_text='100 characters max.')
-    email =  forms.EmailField(widget=forms.EmailInput,label='邮箱',max_length=30)
-    userpwd = forms.CharField(widget=forms.PasswordInput,label='密码',max_length=100)
-    reuserpwd = forms.CharField(widget=forms.PasswordInput,label='确认密码',max_length=100)
-    captcha = CaptchaField(label='验证码',)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'请输入您的用户名'}),
+        label='用户名',
+        max_length=30,
+        help_text='100 characters max.')
+    email =  forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':'请输入您的邮箱'}),
+        label='邮箱',
+        max_length=30)
+    userpwd = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'请输入您的密码'}),
+        label='密码',max_length=100)
+    reuserpwd = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'请重复输入您的密码'}),
+        label='确认密码',max_length=100)
+    captcha = CaptchaField( label='验证码',)
+    #captcha.render('class', 'test')
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='用户名',max_length=30,help_text='100 characters max.')
-    userpwd = forms.CharField(widget=forms.PasswordInput,label='密码',max_length=50)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'请输入您的用户名'}),
+        label='用户名',max_length=30,
+        help_text='100 characters max.')
+    userpwd = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'请输入您的密码'}),
+        label='密码',max_length=50)
 
 class UpdateLoginForm(forms.Form):
     oldpwd =  forms.CharField(widget=forms.PasswordInput,label='当前密码',max_length=100)
